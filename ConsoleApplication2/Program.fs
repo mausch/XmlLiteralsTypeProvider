c33c4d9ac1ebe8ee2021bcb3d6b701fc47962d4b
mausch <mauricioscheffer@gmail.com>
2013-12-02 02:32:42 -0300

    test app

--- ConsoleApplication2/Program.fs
+++ ConsoleApplication2/Program.fs
@@ -7,13 +7,13 @@ let testHtml = """
 	<title><x:text name="title"/></title>
 </head>
 <body>
-	<h1><x:text name="title"/></h1>
+	<h1><x:text name="h1"/></h1>
 </body>
 </html>
 """
 
 type Template = XmlLiterals.Xml<testHtml>
 
-let a = Template(title = "Hello world")
-
-printfn "%A" (a.Render())
+let a = Template(title = "Hello world", h1 = "Bye world")
+let rendered = a.Render()
+printfn "%A" rendered
