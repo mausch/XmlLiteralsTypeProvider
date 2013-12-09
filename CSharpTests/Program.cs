d920916217e12f06961a4dee98ce58dc67b33c60
mausch <mauricioscheffer@gmail.com>
2013-12-08 23:03:21 -0300

    Rename C# tests project

--- CSharpTests/Program.cs
+++ CSharpTests/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharpApp {
+namespace CSharpTests {
     class Program {
         static void Main(string[] args) {
             var template = new Tests.Template(title: "Hello world", mainText: "Bye world");
