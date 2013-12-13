d0e12077a2b51f911b8136fad49807f4acbf69b4
panesofglass <ryan.riley@panesofglass.org>
2013-12-12 20:32:50 -0600

    Use full path to msbuild.

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 @echo off
-msbuild XmlLiteralsTypeProvider.sln || goto :error
-msbuild Tests.sln || goto :error
+C:\Windows\Microsoft.Net\Framework\v4.0.30319\MSBuild.exe XmlLiteralsTypeProvider.sln || goto :error
+C:\Windows\Microsoft.Net\Framework\v4.0.30319\MSBuild.exe Tests.sln || goto :error
 Tests\bin\debug\tests.exe
 
 goto :EOF
