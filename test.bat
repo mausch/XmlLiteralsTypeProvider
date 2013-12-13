4849bee631c6d163fb5556bd30a6b01727e8d42d
mausch <mauricioscheffer@gmail.com>
2013-12-13 02:25:05 -0300

    Fix msbuild path

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 @echo off
-C:\Windows\Microsoft.Net\Framework\v4.0.30319\MSBuild.exe XmlLiteralsTypeProvider.sln || goto :error
-C:\Windows\Microsoft.Net\Framework\v4.0.30319\MSBuild.exe Tests.sln || goto :error
+%WINDIR%\Microsoft.Net\Framework\v4.0.30319\MSBuild.exe XmlLiteralsTypeProvider.sln || goto :error
+%WINDIR%\Microsoft.Net\Framework\v4.0.30319\MSBuild.exe Tests.sln || goto :error
 Tests\bin\debug\tests.exe
 
 goto :EOF
